ba7e19a1d662fb7de94ad261dcc668ba9cb4aeab
jpgtzg <58833781+jpgtzg@users.noreply.github.com>
2025-02-03 13:31:23 -0600

    Removed spell_check json (Redundant)

--- spell_check.py
+++ spell_check.py
@@ -7,13 +7,3 @@ def spell_check(text):
     Spell checks the text
     '''
     return spell(text)
-
-def spell_check_json(json_data: dict):
-    '''
-    Spell checks the JSON data
-    '''
-    for key, value in json_data.items():
-        json_data[key] = spell_check(value)
-
-    return json_data
-
