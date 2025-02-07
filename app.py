ef3c5b6939a7cf8a2ee1f4dfa6ce3571924d8c55
Fortunate Omonuwa <omonuwafortunate@gmail.com>
2025-02-07 13:08:47 +0100

    Remove commented response and test function

--- app.py
+++ app.py
@@ -77,25 +77,6 @@ def experience(index=None):
             experience_obj = Experience(**new_experience)
             data["experience"].append(experience_obj)
             return jsonify({"id": len(data["experience"]) - 1}), 201
-
-
-# Handles deletion of experience
-@app.route('/resume/experience/<int:exp_id>', methods=['DELETE'])
-def delete_experience(exp_id):
-    try:
-        if exp_id < 0 or exp_id >= len(data["experience"]):
-            return jsonify({"message": "Resource doesn't exist"}), 404
-        else:
-            deleted_exp = data['experience'].pop(exp_id)
-           # return jsonify({"message": "Experience Successfully Deleted", "deleted_experience": deleted_exp}), 200
-             return jsonify({"message": "Experience Successfully Deleted"}), 200
-    except Exception as e:
-        print(f"Error :{e} ")
-        return jsonify({"error": "An error occured"}), 500
-    
-
-@app.route('/resume/education', methods=['GET', 'POST'])
-
         except TypeError as e:
             return jsonify({"error": f"Invalid data format: {str(e)}"}), 400
         except Exception as e:
@@ -103,6 +84,19 @@ def delete_experience(exp_id):
 
 
     return jsonify({"error": "Method not allowed"}), 405
+
+
+@app.route('/resume/experience/<int:exp_id>', methods=['DELETE'])
+def delete_experience(exp_id):
+    try:
+        if exp_id < 0 or exp_id >= len(data["experience"]):
+            return jsonify({"message": "Resource doesn't exist"}), 404
+        else:
+            data['experience'].pop(exp_id)
+            return jsonify({"message": "Experience Successfully Deleted"}), 200
+    except Exception as e:
+        return jsonify({"error": f"An error occurred: {str(e)}"}), 500
+    
 
 @app.route("/resume/education", methods=["GET", "POST"])
 
--- test_pytest.py
+++ test_pytest.py
@@ -35,33 +35,6 @@ def test_experience():
 
 
 
-# def test_delete_experience():
-
-#     example_experience = {
-#         "title": "Mechanic",
-#         "company": "Decepticons .Ent",
-#         "start_date": "January 2020",
-#         "end_date": "Present",
-#         "description": "Hail Megatron",
-#         "logo" : "example-log.png"
-#     }
-#     response = app.test_client().post('/resume/experience', json= example_experience)
-#     assert response.status_code == 200
-    
-#     get_all_experiences = app.test_client().get('/resume/experience')
-#     experience_id = len(get_all_experiences.json) - 1
-
-#     delete_response = app.test_client().delete(f'/resume/experience/{experience_id}')
-#     assert delete_response.status_code == 200
-#     deleted_experience = delete_response.json['deleted_experience']
-
-#     assert example_experience == deleted_experience
-
-#     get_exp = app.test_client().get('/resume/experience')
-#     for experience in get_exp.json:
-#         assert experience != deleted_experience
-
-
 def test_education():
     '''
     Add a new education and then get all educations. 
@@ -100,6 +73,3 @@ def test_skill():
 
     response = app.test_client().get('/resume/skill')
     assert response.json[item_id] == example_skill
-
-    response = app.test_client().get(f'/resume/skill/{item_id}')
-    assert response.json == example_skill
